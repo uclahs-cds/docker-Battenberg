144d0de3fd3fbee0dd3f0a6aecd458ce74b7b44b
Mootor <mmootor@ip-0A125967.rhxrlfvjyzbupc03cc22jkch3c.xx.internal.cloudapp.net>
2024-07-22 13:20:05 -0700

    add installer R script that uses pkgdepends

--- installer.R
+++ installer.R
@@ -4,39 +4,26 @@ library(pkgdepends);
 parser <- ArgumentParser();
 
 parser$add_argument(
-    '-r',
-    '--repo-name',
-    help = 'Specify the repo name'
+    '-l',
+    '--lib',
+    help = 'Library path to install the tools'
     );
 parser$add_argument(
-    '-av',
-    '--add-version',
-    help = 'Specify tool version'
+    '-d',
+    '--dependencies',
+    nargs = '+',
+    help = 'List dependencies separated by space'
     );
 
 args <- parser$parse_args();
 
-repo <- args$repo_name;
-version <- args$add_version;
-
-if (!(startsWith(version, 'v'))) {
-    version <- paste('v', version, sep = '')
-    };
-
-tool <- paste(repo, '@' ,version, sep = '');
-
-pkg.download.proposal <- new_pkg_download_proposal(tool);
-pkg.download.proposal$resolve();
-pkg.download.proposal$download();
-
-#Packages and dependencies will be installed in the path below
-lib <- '/usr/lib/R/site-library';
-config <- list(library = lib);
+tools <- args$dependencies;
+lib <- args$lib;
 
 pkg.installation.proposal <- new_pkg_installation_proposal(
-  tool,
-  config = list(library = lib)
-);
+    tools,
+    config = list(library = lib)
+    );
 pkg.installation.proposal$solve();
 pkg.installation.proposal$download();
 pkg.installation.proposal$install();
